61cfc11998e6808578987d0774949908b26f99ea
bernadette99 <fbabagnak@gmail.com>
2023-04-09 20:50:21 +0200

    ajout  d'une class etat et des fonctions trouverDestination, deplacer, estBut, egal, nombreMalMis

--- src/ida_star.py
+++ src/ida_star.py
@@ -1,7 +1,12 @@
 from typing import Sequence, Callable
+from xmlrpc.client import Boolean
+from etat import Etat
+import copy
 
 from game import Game
 from move import Move
+from rod  import Rod
+from etat import Etat
 
 
 class IDAStar():
@@ -12,6 +17,7 @@ class IDAStar():
     get_state_heuristic: Callable[[Game], int] # Fonction heuristique d'un état
 
     threshold: int # Seuil actuel
+    etatBut : Etat
 
     def __init__(
         self, 
@@ -25,6 +31,7 @@ class IDAStar():
         self.is_goal = is_goal
         self.get_state_chidren = get_state_chidren
         self.get_state_heuristic = get_state_heuristic
+        
 
     def find_solution(self) -> list[Move]:
         """Trouver une liste de déplacements pour arriver à un état but. Si impossible, une liste vide de déplacements."""
@@ -34,3 +41,43 @@ class IDAStar():
         
         # TODO implémenter + fonctions intermédiaires
         pass
+    
+    #j'ai du mal avec des variables en anglais donc je vais devoir coder avec le français :)
+
+    def trouverDestination(etat:Etat, piqueI:Rod) ->list[Rod]:
+        PiquesDestinataires = []
+        for i in range(len(Rod)):
+            if i!= piqueI and len(etat.getPique(i)) < Rod.get_size():
+                PiquesDestinataires.append(i)
+        return PiquesDestinataires
+
+
+    def deplacer(etat:Etat, numP1: int, numP2: int) ->Etat:
+        etatCopie = copy.deepcopy(etat)
+        if len(etat.piques[numP1])==0 :
+            return None
+        etatCopie.piques[numP2].put(etatCopie.piques[1].pop())
+
+        return etatCopie
+
+
+    def estBut(etat:Etat, etatBut: Etat) ->Boolean:
+        return egal(etat)
+    
+    def egal(self,etat: Etat) -> Boolean:
+        return etat == self.etatBut
+
+
+    def nombreMalMis(self,etat: Etat) ->int:
+        nbMalPlaces = 0
+        for i in range(etat.lenEtat(etat)):
+            if len(etat.listPique[i]) == len(self.etatBut.listPique[i]):
+                for j in range(self.etatBut.lenEtat(self.etatBut)):
+                    if j >= etat.lenEtat(etat) or etat.piques[i][j]!= self.etatBut.piques[i][j]:
+                        nbMalPlaces +=1
+            else:
+                for j in range(-1, -self.etatBut.lenEtat(self.etatBut)-1, -1):
+                    if -j-1 >= etat.lenEtat(etat) or etat.piques[i][j]!= self.etatBut.piques[i][j]:
+                        nbMalPlaces +=1
+
+
